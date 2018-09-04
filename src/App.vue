e6134175c7212356970f56887212ad405b1b7010
zhangwuji <191933284@qq.com>
2018-09-04 11:39:15 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <MHeader/>
+    <Tab/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  name: 'App',
+  components:{
+    MHeader,
+    Tab
+  }
+}
+</script>
+
+<style>
+#app {
+ 
+  /*margin-top: 60px;*/
+}
+</style>
